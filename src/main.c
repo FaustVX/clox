42689feda24d576210277faee18e34c0e5235e89
FaustVX <jodu035@gmail.com>
2022-11-23 19:03:44 +0100

    Arithmetic Calculator https://craftinginterpreters.com/a-virtual-machine.html#an-arithmetic-calculator

--- src/main.c
+++ src/main.c
@@ -8,7 +8,12 @@ int main(int argc, const char* argv[]) {
   Chunk chunk;
   initChunk(&chunk);
   writeConstant(&chunk, 1.2, 1);
-  writeChunk(&chunk, OP_RETURN, 1);
+  writeConstant(&chunk, 3.4, 1);
+  writeChunk(&chunk, OP_ADD, 1);
+  writeConstant(&chunk, 5.6, 1);
+  writeChunk(&chunk, OP_DIVIDE, 1);
+  writeChunk(&chunk, OP_NEGATE, 1);
+  writeChunk(&chunk, OP_RETURN, 2);
   disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
   freeChunk(&chunk);
--- src/vm.c
+++ src/vm.c
@@ -9,6 +9,12 @@ static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_LONG_CONSTANT() (vm.chunk->constants.values[READ_BYTE() | (READ_BYTE() << 8) | (READ_BYTE() << 16)])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -36,6 +42,26 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_ADD: {
+        BINARY_OP(+);
+        break;
+      }
+      case OP_SUBTRACT: {
+        BINARY_OP(-);
+        break;
+      }
+      case OP_MULTIPLY: {
+        BINARY_OP(*);
+        break;
+      }
+      case OP_DIVIDE: {
+        BINARY_OP(/);
+        break;
+      }
+      case OP_NEGATE: {
+        push(-pop());
+        break;
+      }
       case OP_RETURN: {
         printValue(pop());
         printf("\n");
@@ -44,6 +70,7 @@ static InterpretResult run() {
     }
   }
 
+#undef BINARY_OP
 #undef READ_LONG_CONSTANT
 #undef READ_CONSTANT
 #undef READ_BYTE
