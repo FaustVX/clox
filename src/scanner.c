97fec13df2a0a499ad40ed8d45bfb5fb633e1edf
FaustVX <jodu035@gmail.com>
2022-11-23 22:41:13 +0100

    A Token at a Time https://craftinginterpreters.com/scanning-on-demand.html#a-token-at-a-time

--- src/scanner.c
+++ src/scanner.c
@@ -17,3 +17,34 @@ void initScanner(const char* source) {
   scanner.current = source;
   scanner.line = 1;
 }
+
+static bool isAtEnd() {
+  return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type) {
+  Token token = {
+    .type = type,
+    .start = scanner.start,
+    .length = (int)(scanner.current - scanner.start),
+    .line = scanner.line,
+  };
+  return token;
+}
+
+static Token errorToken(const char* message) {
+  Token token = {
+    .type = TOKEN_ERROR,
+    .start = message,
+    .length = (int)strlen(message),
+    .line = scanner.line,
+  };
+  return token;
+}
+
+Token scanToken() {
+  scanner.start = scanner.current;
+  if (isAtEnd())
+    return makeToken(TOKEN_EOF);
+  return errorToken("Unexpected character.");
+}
