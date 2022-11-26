93f66893be2869c03603e1ac0b5266b24d4eab18
FaustVX <jodu035@gmail.com>
2022-11-26 20:59:56 +0100

    If Statements https://craftinginterpreters.com/jumping-back-and-forth.html#if-statements

--- src/compiler.c
+++ src/compiler.c
@@ -124,12 +124,31 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
   emitByte(byte2);
 }
 
+static int emitJump(uint8_t instruction) {
+  emitByte(instruction);
+  emitByte(0xff);
+  emitByte(0xff);
+  return currentChunk()->count - 2;
+}
+
 static void emitReturn() {
   emitByte(OP_RETURN);
 }
 
 static uint8_t makeConstant(Value value) {
   return (uint8_t)addConstant(currentChunk(), value);
+}
+
+static void patchJump(int offset) {
+  // -2 to adjust for the bytecode for the jump offset itself.
+  int jump = currentChunk()->count - offset - 2;
+
+  if (jump > UINT16_MAX) {
+    error("Too much code to jump over.");
+  }
+
+  currentChunk()->code[offset] = (jump >> 8) & 0xff;
+  currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
 static void emitConstant(Value value) {
@@ -454,6 +473,22 @@ static void expressionStatement() {
   emitByte(OP_POP);
 }
 
+static void ifStatement() {
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition."); 
+
+  int thenJump = emitJump(OP_JUMP_IF_FALSE);
+  emitByte(OP_POP);
+  statement();
+  int elseJump = emitJump(OP_JUMP);
+  patchJump(thenJump);
+  emitByte(OP_POP);
+  if (match(TOKEN_ELSE))
+    statement();
+  patchJump(elseJump);
+}
+
 static void printStatement() {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -503,6 +538,8 @@ static void statement() {
     beginScope();
     block();
     endScope();
+  } else if (match(TOKEN_IF)) {
+    ifStatement();
   } else {
     expressionStatement();
   }
